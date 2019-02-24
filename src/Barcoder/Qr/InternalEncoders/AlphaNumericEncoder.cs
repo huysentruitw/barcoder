97b203a77b8d630583a2bfe69ffb2e3241feea17
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-02-24 20:53:42 +0100

    Add QR alphanumeric encoder with tests

---
+++ src/Barcoder/Qr/InternalEncoders/AlphaNumericEncoder.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Barcoder.Utils;
+
+namespace Barcoder.Qr.InternalEncoders
+{
+    internal sealed class AlphaNumericEncoder : InternalEncoderBase
+    {
+        private const string CharSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ $%*+-./:";
+
+        public override (BitList, VersionInfo) Encode(string content, ErrorCorrectionLevel errorCorrectionLevel)
+        {
+            if (content == null) throw new ArgumentNullException(nameof(content));
+            bool contentLengthIsOdd = content.Length % 2 == 1;
+            int contentBitCount = (content.Length / 2) * 11;
+            if (contentLengthIsOdd)
+                contentBitCount += 6;
+
+            EncodingMode encodingMode = EncodingMode.AlphaNumeric;
+            var versionInfo = VersionInfo.FindSmallestVersionInfo(errorCorrectionLevel, encodingMode, contentBitCount);
+            if (versionInfo == null)
+                throw new InvalidOperationException("Too much data to encode");
+
+            var result = new BitList();
+            result.AddBits((uint)encodingMode, 4);
+            result.AddBits((uint)content.Length, versionInfo.CharCountBits(encodingMode));
+
+            var encoder = new Queue<int>(content.Select(x => CharSet.IndexOf(x)));
+            for (int i = 0; i < content.Length / 2; i++)
+            {
+                int c1 = encoder.Dequeue();
+                int c2 = encoder.Dequeue();
+                if (c1 < 0 || c2 < 0)
+                    throw new InvalidOperationException($"{content} can not be ancoded as {encodingMode}");
+                result.AddBits((uint)(c1 * 45 + c2), 11);
+            }
+
+            if (contentLengthIsOdd)
+            {
+                int c = encoder.Dequeue();
+                if (c < 0)
+                    throw new InvalidOperationException($"{content} can not be ancoded as {encodingMode}");
+                result.AddBits((uint)c, 6);
+            }
+
+            AddPaddingAndTerminator(ref result, versionInfo);
+            return (result, versionInfo);
+        }
+    }
+}
---
+++ tests/Barcoder.Tests/Qr/InternalEncoders/AlphaNumericEncoderTests.cs
@@ -0,0 +1,72 @@
+using System;
+using Barcoder.Qr;
+using Barcoder.Qr.InternalEncoders;
+using Barcoder.Utils;
+using FluentAssertions;
+using Xunit;
+
+namespace Barcoder.Tests.Qr.InternalEncoders
+{
+    public sealed class AlphaNumericEncoderTests
+    {
+        [Fact]
+        public void Encode_AlphaNumericContent_ShouldEncodeCorrectly()
+        {
+            // Arrange
+            var alphaNumericEncoder = new AlphaNumericEncoder();
+
+            // Act
+            (BitList bitList, VersionInfo versionInfo) = alphaNumericEncoder.Encode("HELLO WORLD", ErrorCorrectionLevel.M);
+
+            // Assert
+            bitList.Should().NotBeNull();
+            versionInfo.Should().NotBeNull();
+            versionInfo.Version.Should().Be(1);
+            bitList.GetBytes().Should().BeEquivalentTo(new byte[] { 32, 91, 11, 120, 209, 114, 220, 77, 67, 64, 236, 17, 236, 17, 236, 17 });
+        }
+
+        [Fact]
+        public void Encode_MaximumContentLength_ShouldSucceed()
+        {
+            // Arrange
+            var alphaNumericEncoder = new AlphaNumericEncoder();
+
+            // Act
+            (BitList bitList, VersionInfo versionInfo) = alphaNumericEncoder.Encode(new string('A', 4296), ErrorCorrectionLevel.L);
+
+            // Assert
+            bitList.Should().NotBeNull();
+            versionInfo.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Encode_ExceedMaximumContentLength_ShouldThrowException()
+        {
+            // Arrange
+            var alphaNumericEncoder = new AlphaNumericEncoder();
+
+            // Act
+            Action action = () => alphaNumericEncoder.Encode(new string('A', 4297), ErrorCorrectionLevel.L);
+
+            // Assert
+            action.Should().Throw<InvalidOperationException>()
+                .WithMessage("Too much data to encode");
+        }
+
+        [Theory]
+        [InlineData("ABc", ErrorCorrectionLevel.L)]
+        [InlineData("hello world", ErrorCorrectionLevel.M)]
+        public void Encode_UnknownCharacterInContent_ShouldThrowException(string content, ErrorCorrectionLevel errorCorrectionLevel)
+        {
+            // Arrange
+            var alphaNumericEncoder = new AlphaNumericEncoder();
+
+            // Act
+            Action action = () => alphaNumericEncoder.Encode(content, errorCorrectionLevel);
+
+            // Assert
+            action.Should().Throw<InvalidOperationException>()
+                .WithMessage($"{content} can not be ancoded as AlphaNumeric");
+        }
+    }
+}
