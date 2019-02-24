12cccfa96e2afc7280818b3ed13395554f6e3b66
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2019-02-24 20:53:26 +0100

    Add QR numeric encoder with tests

---
+++ src/Barcoder/Qr/InternalEncoders/NumericEncoder.cs
@@ -0,0 +1,59 @@
+using System;
+using Barcoder.Utils;
+
+namespace Barcoder.Qr.InternalEncoders
+{
+    internal sealed class NumericEncoder : InternalEncoderBase
+    {
+        public override (BitList, VersionInfo) Encode(string content, ErrorCorrectionLevel errorCorrectionLevel)
+        {
+            if (content == null) throw new ArgumentNullException(nameof(content));
+            var contentBitCount = (content.Length / 3) * 10;
+            switch (content.Length % 3)
+            {
+            case 1:
+                contentBitCount += 4;
+                break;
+            case 2:
+                contentBitCount += 7;
+                break;
+            }
+
+            EncodingMode encodingMode = EncodingMode.Numeric;
+            var versionInfo = VersionInfo.FindSmallestVersionInfo(errorCorrectionLevel, encodingMode, contentBitCount);
+            if (versionInfo == null)
+                throw new InvalidOperationException("Too much data to encode");
+
+            var result = new BitList();
+            result.AddBits((uint)encodingMode, 4);
+            result.AddBits((uint)content.Length, versionInfo.CharCountBits(encodingMode));
+
+            for (int i = 0; i < content.Length; i += 3)
+            {
+                string currentContentPart = i + 3 <= content.Length ? content.Substring(i, 3) : content.Substring(i);
+                if (!uint.TryParse(currentContentPart, out uint currentNumericalValue))
+                    throw new InvalidOperationException($"{content} can not be ancoded as {encodingMode}");
+                byte bitCount;
+                switch (currentContentPart.Length % 3)
+                {
+                case 0:
+                    bitCount = 10;
+                    break;
+                case 1:
+                    bitCount = 4;
+                    break;
+                case 2:
+                    bitCount = 7;
+                    break;
+                default:
+                    throw new InvalidOperationException();
+                }
+
+                result.AddBits(currentNumericalValue, bitCount);
+            }
+
+            AddPaddingAndTerminator(ref result, versionInfo);
+            return (result, versionInfo);
+        }
+    }
+}
---
+++ tests/Barcoder.Tests/Qr/InternalEncoders/NumericEncoderTests.cs
@@ -0,0 +1,57 @@
+using System;
+using Barcoder.Qr;
+using Barcoder.Qr.InternalEncoders;
+using Barcoder.Utils;
+using FluentAssertions;
+using Xunit;
+
+namespace Barcoder.Tests.Qr.InternalEncoders
+{
+    public sealed class NumericEncoderTests
+    {
+        [Theory]
+        [InlineData("01234567", new byte[] { 16, 32, 12, 86, 97, 128, 236, 17, 236 })]
+        [InlineData("0123456789012345", new byte[] { 16, 64, 12, 86, 106, 110, 20, 234, 80 })]
+        public void Encode(string content, byte[] expectedBytes)
+        {
+            // Arrange
+            var numericEncoder = new NumericEncoder();
+
+            // Act
+            (BitList bitList, VersionInfo versionInfo) = numericEncoder.Encode(content, ErrorCorrectionLevel.H);
+
+            // Assert
+            bitList.Should().NotBeNull();
+            versionInfo.Should().NotBeNull();
+            bitList.GetBytes().Should().BeEquivalentTo(expectedBytes);
+        }
+
+        [Fact]
+        public void Encode_AlphaNumericContent_ShouldThrowException()
+        {
+            // Arrange
+            var numericEncoder = new NumericEncoder();
+
+            // Act
+            Action action = () => numericEncoder.Encode("foo", ErrorCorrectionLevel.H);
+
+            // Assert
+            action.Should().Throw<InvalidOperationException>()
+                .WithMessage("foo can not be ancoded as Numeric");
+        }
+
+        [Fact]
+        public void Encode_NumericContentTooLong_ShouldThrowException()
+        {
+            // Arrange
+            var numericEncoder = new NumericEncoder();
+
+            // Act
+            Action action = () => numericEncoder.Encode(new string('1', 14297), ErrorCorrectionLevel.H);
+
+            // Assert
+            action.Should().Throw<InvalidOperationException>()
+                .WithMessage("Too much data to encode");
+        }
+    }
+}
