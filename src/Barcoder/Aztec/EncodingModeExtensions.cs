13ce316e7295beca5e52280ab305d3bbb682296f
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2020-02-08 20:52:32 +0100

    Add BitCount extension method for EncodingMode

---
+++ src/Barcoder/Aztec/EncodingModeExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace Barcoder.Aztec
+{
+    internal static class EncodingModeExtensions
+    {
+        public static byte BitCount(this EncodingMode mode)
+            => mode == EncodingMode.Digit ? (byte)4 : (byte)5;
+    }
+}
