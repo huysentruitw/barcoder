1c8361fd8180ceb7c0c062bb2dfbe3371e5d4acc
Edvin Weijmers <edvin@weijmers.com>
2020-08-20 09:40:17 +0200

    Render Content for EAN
    Render content for EAN8 and EAN13

--- src/Barcoder.Renderer.Svg/SvgRenderer.cs
+++ src/Barcoder.Renderer.Svg/SvgRenderer.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Linq;
+using System.Xml.XPath;
 using Barcoder.Renderers;
 using SvgLib;
 
@@ -12,7 +14,21 @@ namespace Barcoder.Renderer.Svg
             barcode = barcode ?? throw new ArgumentNullException(nameof(barcode));
             outputStream = outputStream ?? throw new ArgumentNullException(nameof(outputStream));
             if (barcode.Bounds.Y == 1)
-                Render1D(barcode, outputStream);
+            {
+                if (barcode.Metadata.CodeKind == BarcodeType.EAN8C)
+                {
+                    Render1DEan8C(barcode, outputStream);
+                }
+                else if (barcode.Metadata.CodeKind == BarcodeType.EAN13C)
+                {
+                    Render1DEan13C(barcode, outputStream);
+                }
+                else
+                {
+                    Render1D(barcode, outputStream);
+                }
+                
+            }
             else if (barcode.Bounds.Y > 1)
                 Render2D(barcode, outputStream);
             else
@@ -62,7 +78,121 @@ namespace Barcoder.Renderer.Svg
 
                 prevBar = true;
             }
+            
+            document.Save(outputStream);
+        }
 
+        private static void Render1DEan8C(IBarcode barcode, Stream outputStream)
+        {
+            var document = SvgDocument.Create();
+            document.ViewBox = new SvgViewBox
+            {
+                Left = 0,
+                Top = 0,
+                Width = barcode.Bounds.X + 2 * barcode.Margin,
+                Height = 55
+            };
+            document.Fill = "#FFFFFF";
+            document.Stroke = "#000000";
+            document.StrokeWidth = 1;
+            document.StrokeLineCap = SvgStrokeLineCap.Butt;
+
+            var prevBar = false;
+            var longerBars = new int[] { 0, 2, 32, 34, 64, 66 };
+            for (var x = 0; x < barcode.Bounds.X; x++)
+            {
+                if (!barcode.At(x, 0))
+                {
+                    prevBar = false;
+                    continue;
+                }
+
+                SvgLine line;
+                int lineHeight = longerBars.Contains(x) ? 55 : 48;
+                if (prevBar)
+                {
+                    line = document.AddLine();
+                    line.StrokeWidth = 1.5;
+                    line.X1 = line.X2 = x + barcode.Margin - 0.25;
+                    line.Y1 = 0;
+                    line.Y2 = lineHeight;
+                }
+                else
+                {
+                    line = document.AddLine();
+                    line.X1 = line.X2 = x + barcode.Margin;
+                    line.Y1 = 0;
+                    line.Y2 = lineHeight;
+                }
+
+                prevBar = true;
+            }
+
+            AddText(document, 18, 54.5D, barcode.Content.Substring(0, 4));
+            AddText(document, 50, 54.5D, barcode.Content.Substring(4));
+            document.Save(outputStream);
+        }
+
+        private static void AddText(SvgDocument doc, double x, double y, string t)
+        {
+            var text = doc.AddText();
+            text.FontFamily = "arial";
+            text.Text = t;
+            text.X = x;
+            text.Y = y;
+            text.StrokeWidth = 0;
+            text.Fill = "#000000";
+            text.FontSize = 8D;
+        }
+
+        private static void Render1DEan13C(IBarcode barcode, Stream outputStream)
+        {
+            var document = SvgDocument.Create();
+            document.ViewBox = new SvgViewBox
+            {
+                Left = 0,
+                Top = 0,
+                Width = barcode.Bounds.X + 2 * barcode.Margin,
+                Height = 55
+            };
+            document.Fill = "#FFFFFF";
+            document.Stroke = "#000000";
+            document.StrokeWidth = 1;
+            document.StrokeLineCap = SvgStrokeLineCap.Butt;
+
+            var prevBar = false;
+            var longerBars = new int[] { 0, 2, 46, 48, 92, 94 };
+            for (var x = 0; x < barcode.Bounds.X; x++)
+            {
+                if (!barcode.At(x, 0))
+                {
+                    prevBar = false;
+                    continue;
+                }
+
+                SvgLine line;
+                int lineHeight = longerBars.Contains(x) ? 55 : 48;
+                if (prevBar)
+                {
+                    line = document.AddLine();
+                    line.StrokeWidth = 1.5;
+                    line.X1 = line.X2 = x + barcode.Margin - 0.25;
+                    line.Y1 = 0;
+                    line.Y2 = lineHeight;
+                }
+                else
+                {
+                    line = document.AddLine();
+                    line.X1 = line.X2 = x + barcode.Margin;
+                    line.Y1 = 0;
+                    line.Y2 = lineHeight;
+                }
+
+                prevBar = true;
+            }
+            AddText(document, 4, 54.5D, barcode.Content.Substring(0, 1));
+            AddText(document, 21, 54.5D, barcode.Content.Substring(1, 6));
+            AddText(document, 67, 54.5D, barcode.Content.Substring(7));
             document.Save(outputStream);
         }
 
